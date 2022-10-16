71b214c864856d2e59c1c159ba3861c1373b945e
resh-ie <reshmamabraham@gmail.com>
2022-10-16 16:45:31 +1100

    commenting out nav bar stories

--- src/stories/NavBar/NavBar.stories.tsx
+++ src/stories/NavBar/NavBar.stories.tsx
@@ -1,17 +1,19 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+// import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import NavBar from "../../components/NavBar/NavBar";
-import { NavData } from "../../components/NavBar/NavData";
-export default {
-  title: "Components/NavBar",
-  component: NavBar,
-  argTypes: {
-    navData: { control: "object" },
-  },
-} as ComponentMeta<typeof NavBar>;
+// import NavBar from "../../components/NavBar/NavBar";
+// import { NavData } from "../../components/NavBar/NavData";
+// export default {
+//   title: "Components/NavBar",
+//   component: NavBar,
+//   argTypes: {
+//     navData: { control: "object" },
+//   },
+// } as ComponentMeta<typeof NavBar>;
 
-const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
-export const Default = Template.bind({});
-Default.args = {
-  navData: NavData
-};
+// const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
+// export const Default = Template.bind({});
+// Default.args = {
+//   navData: NavData
+// };
+
+export default {};
